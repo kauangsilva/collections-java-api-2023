0e9ae6ccd7da9b9fcdc67c44a18499aedf248e40
Kauan Geraldo <kauan_nx@hotmail.com>
2023-10-02 14:58:40 -0300

    Resolvendo Desafio Ordenação de Pessoas

---
+++ src/main.java/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,57 @@
+package main.java.list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        if (!pessoaList.isEmpty()) {
+            Collections.sort(pessoasPorIdade);
+            return pessoasPorIdade;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public List<Pessoa> ordenarPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        if (!pessoaList.isEmpty()) {
+            Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+            return pessoasPorAltura;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoPessoas
+        OrdenacaoPessoa ordenacaoPessoas = new OrdenacaoPessoa();
+
+        // Adicionando pessoas à lista
+        ordenacaoPessoas.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoas.pessoaList);
+
+
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+
+
+    }
+}
--- src/main.java/list/Pesquisa/CatalogoLivros.java
+++ src/main.java/list/Pesquisa/CatalogoLivros.java
@@ -54,7 +54,7 @@ public class CatalogoLivros {
     public static void main(String[] args) {
         CatalogoLivros catalogoLivros = new CatalogoLivros();
         catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2021);
-        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
         catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
         catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
         catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
